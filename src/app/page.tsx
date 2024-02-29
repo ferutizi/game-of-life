4ffafe83bfd919859f8ea3bf30240c83e2d92362
ferutizi <fer.utizi@gmail.com>
2024-02-29 17:41:48 -0300

    fixed rule: death cell revive if alivedNeighbor == 3. some styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,21 @@
 'use client'
 
-import { time } from "console";
 import { useState, useEffect } from "react";
 
-type Box = { 
+type Cell = { 
   row: number,
   col: number,
   isAlive: boolean
 }
 
-type Row = Box[]
-type Grid = Box[][]
+type Row = Cell[]
+type Grid = Cell[][]
 
 export default function Home() {
   //Grid 20x20
-  const gridRows = 60
-  const gridCols = 120
-  const [grid, setGrid] = useState<Grid>([[{row: 1, col: 1, isAlive: false}]]) 
+  const gridRows = 42
+  const gridCols = 90
+  const [grid, setGrid] = useState<Grid>([[]]) 
   const [isPlaying, setIsPlaying] = useState<boolean>(false) 
 
   const generateRow = (cols: number, row: number) => {
@@ -42,7 +41,7 @@ export default function Home() {
     return grid
   }
 
-  const handleLife = (row: Box['row'], col: Box['col'], isAlive: Box['isAlive']) => {
+  const handleLife = (row: Cell['row'], col: Cell['col'], isAlive: Cell['isAlive']) => {
     setGrid((prevGrid) => {
       if (prevGrid && prevGrid[row] && prevGrid[row][col]) {
         const newGrid = [...prevGrid];
@@ -53,12 +52,11 @@ export default function Home() {
     });
   }
 
-  //contador
   // si 2 o 3 permanece viva
   // +3 o -2 muere
   // muerta con +3 revive
 
-  const liveGame = (row: Box['row'], col: Box['row'], alive: Box['isAlive']) => {
+  const liveGame = (row: Cell['row'], col: Cell['col'], alive: Cell['isAlive']) => {
     let alivedNeighbor = 0
 
     for (let i = Math.max(0, row - 1); i <= Math.min(gridCols - 1, row + 1); i++) {
@@ -72,7 +70,7 @@ export default function Home() {
     if(alive) {
       return alivedNeighbor >= 2 && alivedNeighbor <= 3
     } else {
-      return alivedNeighbor >= 3
+      return alivedNeighbor === 3
     }
   }
 
@@ -94,7 +92,7 @@ export default function Home() {
     };
   
     if (isPlaying) {
-      time = setInterval(updateGridCallback, 500);
+      time = setInterval(updateGridCallback, 50);
     }
   
     return () => {
@@ -104,26 +102,31 @@ export default function Home() {
     
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Game of life</h1>
-      <div className="flex gap-1">
-        {grid?.map(e => 
-          <div className="flex flex-col gap-1">
-            {
-              e.map(x =>   
-                <div 
-                key={`${x.row}-${x.col}`}
-                {...{ "data-isalive": x.isAlive}}
-                onClick={() => handleLife(x.row, x.col, x.isAlive)}
-                className={`w-5 h-5 borde hover:bg-slate-500 border-black cursor-pointer ${x.isAlive ? 'bg-slate-900' : 'bg-slate-300'}`}
-                >
-                </div>
-              )
-            }
-          </div>
-        )}
-      </div>
-        <button onClick={() => setIsPlaying(!isPlaying)}>Start</button>
+    <main className="flex h-screen flex-col items-center w-full bg-stone-900">
+      <header className="flex gap-4 items-center bg-slate-300 text-black h-16 w-screen justify-center">
+        <h1>Game of life</h1>
+        <button onClick={() => setIsPlaying(!isPlaying)}>{isPlaying ? 'Stop' : 'Start'}</button>
+      </header>
+      { grid.length > 1 
+      ?<div className="flex px-12 my-auto">
+          {grid?.map(e => 
+            <div className="flex flex-col ">
+              {
+                e.map(x =>   
+                  <div 
+                  key={`${x.row}-${x.col}`}
+                  {...{ "data-isalive": x.isAlive}}
+                  onClick={() => handleLife(x.row, x.col, x.isAlive)}
+                  className={`w-5 h-5 hover:bg-stone-300 border border-black cursor-pointer ${x.isAlive ? 'bg-stone-300' : 'bg-gray-600'}`}
+                  >
+                  </div>
+                )
+              }
+            </div>
+          )}
+        </div>
+      : <div>Loading...</div> 
+      }
     </main>
   );
 }
